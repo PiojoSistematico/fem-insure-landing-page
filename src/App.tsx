f7ff0e903925dad5a12b5b85aab70e8cb403a394
Jorge Saa <piojo.sistematico@gmail.com>
2023-08-31 20:43:14 -0400

    added mobile css

--- src/App.tsx
+++ src/App.tsx
@@ -11,98 +11,94 @@ import twitter from "./assets/images/icon-twitter.svg";
 import pinterest from "./assets/images/icon-pinterest.svg";
 import instagram from "./assets/images/icon-instagram.svg";
 
+import Menu from "./components/Menu";
+
 function App() {
   return (
-    <main>
-      <header>
+    <>
+      <Menu></Menu>
+      <main>
+        <section title="hero" className="hero-section">
+          <picture className="hero-container">
+            <img src={heroMobile} alt="" className="mobile hero" />
+            <img src={heroDesktop} alt="" className="desktop hero" />
+          </picture>
+          <div className="hero-text">
+            <h1>Humanizing your insurance.</h1>
+            <p>
+              Get your life insurance coverage easier and faster. We blend our
+              expertise and technology to help you find the plan that’s right
+              for you. Ensure you and your loved ones are protected.
+            </p>
+            <button>View plans</button>
+          </div>
+        </section>
+        <section title="description" className="description-section">
+          <h2>We’re different</h2>
+          <div className="card-section">
+            <div className="card">
+              <picture className="img-card-container">
+                <img src={card1} alt="" />
+              </picture>
+              <h3>Snappy Process</h3>
+              <p>
+                Our application process can be completed in minutes, not hours.
+                Don’t get stuck filling in tedious forms.
+              </p>
+            </div>
+            <div className="card">
+              <picture className="img-card-container">
+                <img src={card2} alt="" />
+              </picture>
+              <h3>Affordable Prices</h3>
+              <p>
+                We don’t want you worrying about high monthly costs. Our prices
+                may be low, but we still offer the best coverage possible.
+              </p>
+            </div>
+            <div className="card">
+              <picture className="img-card-container">
+                <img src={card3} alt="" />
+              </picture>
+              <h3>People First</h3>
+              <p>
+                Our plans aren’t full of conditions and clauses to prevent
+                payouts. We make sure you’re covered when you need it.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="find-out-more-section">
+          <div className="find-out-more-div">
+            <h1>Find out more about how we work</h1>
+            <button>How we work</button>
+          </div>
+        </section>
+      </main>
+
+      <footer>
         <picture>
           <img src={logo} alt="" />
         </picture>
 
-        <nav>
+        <div className="social-links">
+          <a href="#">
+            <img src={facebook} alt="" />
+          </a>
+          <a href="#">
+            <img src={twitter} alt="" />
+          </a>
+          <a href="#">
+            <img src={pinterest} alt="" />
+          </a>
+          <a href="#">
+            <img src={instagram} alt="" />
+          </a>
+        </div>
+
+        <nav className="footer-nav">
+          <h3>Our company</h3>
           <ul>
-            <li>
-              <a href="">How we work</a>
-            </li>
-            <li>
-              <a href="">Blog</a>
-            </li>
-            <li>
-              <a href="">Account</a>
-            </li>
-            <li>
-              <a href="">View plans</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <section title="hero" className="hero-section">
-        <h1>Humanizing your insurance.</h1>
-        <p>
-          Get your life insurance coverage easier and faster. We blend our
-          expertise and technology to help you find the plan that’s right for
-          you. Ensure you and your loved ones are protected.
-        </p>
-        <button>View plans</button>
-        <picture>
-          <img src={heroMobile} alt="" className="mobile" />
-          <img src={heroDesktop} alt="" className="desktop" />{" "}
-        </picture>
-      </section>
-      <section title="description" className="description-section">
-        <h2>We’re different</h2>
-        <div className="card-section">
-          <picture>
-            <img src={card1} alt="" />
-          </picture>
-          <h3>Snappy Process</h3>
-          <p>
-            Our application process can be completed in minutes, not hours.
-            Don’t get stuck filling in tedious forms.
-          </p>
-        </div>
-        <div className="card-section">
-          <picture>
-            <img src={card2} alt="" />
-          </picture>
-          <h3>Affordable Prices</h3>
-          <p>
-            We don’t want you worrying about high monthly costs. Our prices may
-            be low, but we still offer the best coverage possible.
-          </p>
-        </div>
-        <div className="card-section">
-          <picture>
-            <img src={card3} alt="" />
-          </picture>
-          <h3>People First</h3>
-          <p>
-            Our plans aren’t full of conditions and clauses to prevent payouts.
-            We make sure you’re covered when you need it.
-          </p>
-        </div>
-        <div className="find-out-more">
-          <h2>Find out more about how we work</h2>
-          <button>How we work</button>
-        </div>
-      </section>
-      <footer>
-        <div>
-          <picture>
-            <img src={logo} alt="" />
-          </picture>
-          <div className="social-links">
-            <a href="">
-              <img src={facebook} alt="" />
-              <img src={twitter} alt="" />
-              <img src={pinterest} alt="" />
-              <img src={instagram} alt="" />
-            </a>
-          </div>
-        </div>
-        <div className="footer-grid">
-          <ul>
-            <li>Our company</li>
             <li>
               <a href="">How we work</a>
             </li>
@@ -116,8 +112,8 @@ function App() {
               <a href="">Reviews</a>
             </li>
           </ul>
+          <h3>Help me</h3>
           <ul>
-            <li>Help me</li>
             <li>
               <a href="">FAQ</a>
             </li>
@@ -131,8 +127,8 @@ function App() {
               <a href="">Cookies</a>
             </li>
           </ul>
+          <h3>Contact</h3>
           <ul>
-            <li>Contact</li>
             <li>
               <a href="">Sales</a>
             </li>
@@ -143,8 +139,8 @@ function App() {
               <a href="">Live chat</a>
             </li>
           </ul>
+          <h3>Others</h3>
           <ul>
-            <li>Others</li>
             <li>
               <a href="">Careers</a>
             </li>
@@ -155,9 +151,9 @@ function App() {
               <a href="">Licenses</a>
             </li>
           </ul>
-        </div>
+        </nav>
       </footer>
-    </main>
+    </>
   );
 }
 
